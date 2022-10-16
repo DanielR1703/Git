404e9f13ae4e294d301ee2ab43feb5e777e67ac1
Daniel Ramos <drb.ramos@campus.fct.unl.pt>
2022-10-16 22:24:55 +0100

    Vários A's com for loop

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Bom Dia!");
+        for (int i = 0; i < 20; i++) {
+            System.out.println("a");
+        }
     }
 }
